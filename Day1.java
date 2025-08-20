9d9ee5a09a3a004773f576297a3d0fc1505ecd1e
Manisha2204 <118578660+Manisha2204@users.noreply.github.com>
2025-08-20 11:18:26 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+// reverse the string without the string builder
+public class Day1 {
+    public static void main(String[] args) {
+        // take input using the Scanner class
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        // take the character of the input using charAt()
+        // print each character one by one in reverse order
+        for (int i=input.length()-1;i>=0;i--){
+            char ch = input.charAt(i);
+            System.out.print(ch);
+        }
+    }
+
+}
